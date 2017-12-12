36e615177d71789f857d17c82d0218dfe768c3aa
san_kov <sanoktabx@gmail.com>
2017-12-12 23:51:06 +0600

    =add delete script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,18 +1,17 @@
 const {MongoClient, ObjectID} = require('mongodb');
 
-
-
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
         return console.log('Unable to connect to MongoDB server');
     }
     console.log('Connected to MongoDB');
 
-    db.collection('Users').findOne({username: 'Mike'}).then((count) => {
-        console.log(count);
-    }, (err) => {
-        console.log("Unable to fetch data");
+    db.collection('Users').findOneAndDelete({
+        _id: new ObjectID("5a1fa116ac1e7717204858c9")
+    }).then((result) => {
+        console.log(result);
     });
 
+   
     db.close();
-});
+}); 
