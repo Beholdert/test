0e81961eb71f91c7259180f234ee2062f67a6120
san_kov <sanoktabx@gmail.com>
2017-12-13 00:16:12 +0600

    =update docs

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,8 +6,31 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB');
 
-    db.collection('Users').findOneAndDelete({
-        _id: new ObjectID("5a1fa116ac1e7717204858c9")
+    // db.collection('Users').findOneAndDelete({
+    //     _id: new ObjectID("5a1fa116ac1e7717204858c9")
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Users').insert({
+    //     name: 'John', 
+    //     age: 20, 
+    //     hobby: 'coding'
+    // }).then((result) => {
+    //     console.log(result);
+    // })
+
+    db.collection('Users').findOneAndUpdate({
+        name: 'John'
+    }, {
+        $set: {
+            name: 'Alex'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
     }).then((result) => {
         console.log(result);
     });
